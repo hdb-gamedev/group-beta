d3076e0e3145eaea700d9b383fd94e43878215e3
Jonah Halbisen <truffaka04@gmail.com>
2022-01-11 16:00:32 -0500

    added blue ball with gravity

---
+++ project/Rigidball.gd
@@ -0,0 +1,24 @@
+extends RigidBody
+
+var force = Vector3(0, 0, 0)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _physics_process(delta):
+	#add_central_force(Vector3(1000, 1000, 1000))
+	pass
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
